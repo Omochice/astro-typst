6b6a1d0fe669f8965e10d48ca6aa5c562312d85c
Omochice <mochice.mls.ntl@gmail.com>
2025-07-07 03:42:26 +0900

    style: reformat

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { typst } from 'astro-typst';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import { typst } from "astro-typst";
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [
     mdx(),
     sitemap(),
@@ -16,4 +16,3 @@ export default defineConfig({
     }),
   ],
 });
-
